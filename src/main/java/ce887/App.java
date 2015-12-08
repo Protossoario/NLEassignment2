516713f4fdec87819453693c8de352beb151a766
USER END <USER END@100.75.107.0>
2015-12-08 23:55:03 +0000

    Tokenized Text and output of each step in a textfiles.

--- src/main/java/ce887/App.java
+++ src/main/java/ce887/App.java
@@ -1,19 +1,38 @@
 package ce887;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException
     {
     	HTMLParser parser = new HTMLParser();
         String text = parser.parseHTML("The Udo.html");
+        PrintWriter outText = new PrintWriter("textFromHTML.txt");
+        outText.println(text);
     	//System.out.println(text);
         
+        StringTokenizer tokenizer = new StringTokenizer();
+        String tokenizedText = tokenizer.tokenize(text);
+        PrintWriter outTokens = new PrintWriter("tokenizedText.txt");
+        outTokens.println(tokenizedText);
+        //System.out.println(tokenizedText);
+        
         POSTagger postagger = new POSTagger();
-        String taggedText = postagger.tag(text);
-        System.out.println(taggedText);
+        String taggedText = postagger.tag(tokenizedText);
+        PrintWriter outTagged = new PrintWriter("taggedText.txt");
+        outTagged.println(taggedText);
+        //System.out.println(taggedText);
+        
+        
+        outText.close();
+        outTokens.close();
+        outTagged.close();
     }
 }
---
+++ src/main/java/ce887/StringTokenizer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ce887;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+/**
+ *
+ * @author USER END
+ */
+public class StringTokenizer {
+    
+    public String tokenize(String text) {
+        
+        InputStream modelIn = null;
+        
+        TokenizerModel model;
+        
+        try {
+            modelIn = new FileInputStream("en-token.bin");
+            model = new TokenizerModel(modelIn);
+        }
+        catch (IOException e) {
+            //Model loading failure, handle error
+            model = null;
+            e.printStackTrace();
+        }
+        finally {
+            if (modelIn != null) {
+                try {
+                    modelIn.close();
+                }
+                catch (IOException e) {
+                }
+            }
+        }
+        
+        Tokenizer tokenizer = new TokenizerME(model);
+        String [] tokens = tokenizer.tokenize(text);
+        StringBuilder tokenizedText = new StringBuilder();
+        
+        
+        for (int i = 0; i < tokens.length; i++) {
+            tokenizedText.append(tokens[i] + " ");
+        }
+        
+        return tokenizedText.toString();
+    }
+    
+}
